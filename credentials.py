7d15773c3dd265b3eff22e84c0ab3f515a456252
MiroslavaRo <72034705+MiroslavaRo@users.noreply.github.com>
2023-06-29 18:25:18 +0300

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-URL = "https://t.me/super_car_service_bot"
+URL = "YOUR URL"
 bot = telebot.TeleBot(BOT_TOKEN)
 
 MAX_CARS = 10
